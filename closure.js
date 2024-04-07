b422c1516f4c5c76a094f4a3e431192e7c5a55cf
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-04-08 05:49:13 +0600

    closure added

---
+++ closure.js
@@ -0,0 +1,25 @@
+function stopWatch() {
+  let counter = 0;
+  return function () {
+    counter++;
+    return counter;
+  }
+}
+
+const watch1 = stopWatch();
+console.log(watch1());
+console.log(watch1());
+console.log(watch1());
+console.log(watch1());
+
+const watch2 = stopWatch();
+console.log(watch2());
+console.log(watch2());
+console.log(watch2());
+console.log(watch2());
+
+const watch3 = stopWatch();
+console.log(watch3());
+console.log(watch3());
+console.log(watch3());
+console.log(watch3());
